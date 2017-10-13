4888eb0fa1203334414a6e23283ddcc64c462ab4
Mehmet Dogan <info@intelligentworks.ch>
2017-10-13 22:34:15 +0200

    delete old functions

--- js/main.js
+++ js/main.js
@@ -5,29 +5,3 @@
 
 "use strict";
 
-// Please us test.js for this assignment
-
-hideAllImages();
-showAllImages();
-changeHeadingToTheBestCollection();
-makeHrLineElementBolder();
-changeBackgroundColorOfEachProductTitleWithDifferentColor();
-removeBuyItemButtons();
-removeLastThreeItemsFromView();
-makeTenPercentPriceReductionOnAllProducts();
-renameTheProductShirtToFishShirt();
-renameTheFirstRockItemToBirdRock();
-addFiveStarsToTheProductShirt();
-changeTheTitleNameWithARandomName();
-colorTheStarsOfTheThirdProductWithGreen();
-resetLastTwoImagesToUrl();
-constantlyChangeThePriceAndIncrementItByOneInEachThreeSeconds();
-showTheBuyItemAgainWithAGreenBackgroundGrayBorderAndThinShadow();
-addAnEventHandlerToTheBuyItemButtonsAndAfterClickShowAlert();
-bringBackTheInitialImageAgainInsteadOfUrl();
-changeEveryProductDescriptionToAnyTextWithAtLeast50Characters();
-randomlyChangeAllOfThePrices();
-markTheBackgroundWithTheColorYellowOfTheTwoCheapestProducts();
-sortAllOfTheProductsAscendantlyBasedOnTheNewPrices();
-addAnMouseOverEventOnlyTheHighestTwoProductsWhichLogsInConsoleThePrice();
-addThreeNewProductsToTheEndOfTheProductList();
