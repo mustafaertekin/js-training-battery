33e489cd449ffc52a4e8f74c4920a7a05f4011fd
mustafaertekin <mustafa014@gmail.com>
2017-10-18 01:44:00 +0200

    some class names re-organised

--- test/tests.js
+++ test/tests.js
@@ -1,64 +1,59 @@
-
 describe("Battery Charge - Test => ", () => {
     it('should test the function changeColor for case 0', () => {
         changeColor(0);
         let color = 0;
-        if ($('#birinci').css('background-color') == 'rgb(255, 255, 255)') { 
+        if ($('#firstCell').css('background-color') == 'rgb(255, 255, 255)') {
             color = color + 1;
         }
-        
-        if ($('#ikinci').css('background-color') == 'rgb(255, 255, 255)') { 
+
+        if ($('#secondCell').css('background-color') == 'rgb(255, 255, 255)') {
             color = color + 1;
         }
-        
-        if ($('#ucuncu').css('background-color') == 'rgb(255, 255, 255)') { 
+
+        if ($('#thirdCell').css('background-color') == 'rgb(255, 255, 255)') {
             color = color + 1;
         }
-        
-        if ($('#dorduncu').css('background-color') == 'rgb(255, 255, 255)') { 
+
+        if ($('#fourthCell').css('background-color') == 'rgb(255, 255, 255)') {
             color = color + 1;
         }
-        
+
         // If all the battery cells are white, it must be 1+1+1+1=4
-        
+
         color.should.be.equal(4);
     });
-    
+
     it('should test the function changeColor for case 1', () => {
         changeColor(1);
         let color = 0;
-        console.log($('#birinci').css('background-color'));
-        if ($('#birinci').css('background-color') == 'rgb(255, 0, 0)') { 
+        if ($('#firstCell').css('background-color') == 'rgb(255, 0, 0)') {
             color = 1;
         }
         color.should.be.equal(1);
     });
-    
+
     it('should test the function changeColor case for 2', () => {
         changeColor(2);
         let color = 0;
-        console.log($('#ikinci').css('background-color'));
-        if ($('#ikinci').css('background-color') == 'rgb(255, 165, 0)') { 
+        if ($('#secondCell').css('background-color') == 'rgb(255, 165, 0)') {
             color = 1;
         }
         color.should.be.equal(1);
     });
-    
+
     it('should test the function changeColor for case 3', () => {
         changeColor(3);
         let color = 0;
-        console.log($('#ucuncu').css('background-color'));
-        if ($('#ucuncu').css('background-color') == 'rgb(255, 255, 0)') { 
+        if ($('#thirdCell').css('background-color') == 'rgb(255, 255, 0)') {
             color = 1;
         }
         color.should.be.equal(1);
     });
-    
+
     it('should test the function changeColor for case 4', () => {
         changeColor(4);
         let color = 0;
-        console.log($('#dorduncu').css('background-color'));
-        if ($('#dorduncu').css('background-color') == 'rgb(0, 128, 0)') { 
+        if ($('#fourthCell').css('background-color') == 'rgb(0, 128, 0)') {
             color = 1;
         }
         color.should.be.equal(1);
